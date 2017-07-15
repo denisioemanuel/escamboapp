04a42535f3bbcf354cba54c7c4bb67af3eb56ba2
Denisio MacBook <denisio@ymail.com>
2017-07-14 22:44:52 -0300

    Aula 037 - Corrigindo o Active Record Scope

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,13 @@
 class Admin < ApplicationRecord
+  #Enums
   enum role: [:full_access, :restricted_access]
-
-  scope :with_full_access, -> { where(role: 'restricted_access') }
+  #Scoopes
+  scope :with_full_access, -> { where(role: 'full_access') }
+  scope :with_restricted_access, -> { where(role: 'restricted_access') }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
 end
