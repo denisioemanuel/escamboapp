2a8fa9236bb60ec26d0e0dd5beeeba9487a47e76
Denisio MacBook <denisio@ymail.com>
2017-08-14 21:29:20 -0300

    Aula 64  / Scopes com parâmetros

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,7 +3,8 @@ class Ad < ApplicationRecord
   belongs_to :member
 
   #scope
-  scope :last_six, -> { order(created_at: :asc).limit(6) }
+  scope :descending_order, -> (quantity = 10) { order(created_at: :asc).limit(quantity) }
+  scope :to_the, -> (member) { where(member: member) }
 
   #paperclip
   has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
