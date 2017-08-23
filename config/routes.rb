14114ce95f903a41472326541d9034c9f8ac437a
Denisio MacBook <denisio@ymail.com>
2017-08-22 22:26:22 -0300

    Aula 072 - Redirecionando rotas com Devise

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   devise_for :admins, :skip => [:registration]
-  devise_for :members
+  devise_for :members , controllers: { sessions: 'members/sessions' }
   get 'home/index'
   root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
