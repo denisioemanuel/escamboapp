774666f5ea55ed53bf24b3242a9527576917c056
Denisio MacBook <denisio@ymail.com>
2017-11-06 21:44:44 -0300

    Aula 101 - Nested Attributes com Devise

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,10 @@ Rails.application.routes.draw do
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   devise_for :admins, :skip => [:registration]
-  devise_for :members , controllers: { sessions: 'members/sessions' }
+  devise_for :members , controllers: {
+    sessions: 'members/sessions',
+    registrations: 'members/registrations' 
+  }
   get 'home/index'
   root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
