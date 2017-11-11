0ecf600e6f98fa594c438a0a60715ccce50ddaa3
Denisio MacBook <denisio@ymail.com>
2017-11-11 09:28:36 -0300

    Aula 105 - Validando Nested Attributes e Validações Personalizadas

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,14 +1,28 @@
 class Member < ApplicationRecord
   #Ratyrate gem
   ratyrate_rater
-  
+
   #Associations
   has_many :ads
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
+  #Validations
+  validate :nested_attributes
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+    def nested_attributes
+      if nested_attributes_blank?
+        errors.add(:base, "Necessário preencher o Primeiro e o Segundo Nome")
+      end
+    end
+
+    def nested_attributes_blank?
+      profile_member.first_name.blank? || profile_member.second_name.blank?
+    end
 end
